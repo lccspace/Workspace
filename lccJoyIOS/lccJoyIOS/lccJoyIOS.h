116a8d43f6040cc55cc3269dd1a2a2fc0f3673ea
lcc <licc73@126.com>
2014-03-12 17:17:46 +0800

    iOS

---
+++ lccJoyIOS/lccJoyIOS/lccJoyIOS.h
@@ -0,0 +1,13 @@
+//
+//  lccJoyIOS.h
+//  lccJoyIOS
+//
+//  Created by mcc on 14-3-12.
+//  Copyright (c) 2014年 lcc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface lccJoyIOS : NSObject
+
+@end
---
+++ lccJoyIOS/lccJoyIOS/lccJoyIOS.m
@@ -0,0 +1,13 @@
+//
+//  lccJoyIOS.m
+//  lccJoyIOS
+//
+//  Created by mcc on 14-3-12.
+//  Copyright (c) 2014年 lcc. All rights reserved.
+//
+
+#import "lccJoyIOS.h"
+
+@implementation lccJoyIOS
+
+@end
